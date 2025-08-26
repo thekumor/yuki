d461b961cb868db266a9c3072b6eee236f18adb2
Sebastian Kumór <thekumor@gmail.com>
2025-08-26 10:45:33 +0200

    Bot login
    + Added bot login functionality.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-var foo = 5;
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log('Bot logged!');
+});
+
+client.login(token);
+
