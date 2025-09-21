7917ff4da630ed1586c5853313d18b4679011785
Sebastian Kumór <thekumor@gmail.com>
2025-09-21 10:21:24 +0200

    Update index.js
    + Added command refreshing.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // 
 // ================================================
 
-const { Client, Events, GatewayIntentBits, Collection, MessageFlags } = require('discord.js');
+const { REST, Routes, Client, Events, GatewayIntentBits, Collection, MessageFlags } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 const { token } = require('./config.json');
@@ -14,6 +14,8 @@ const { token } = require('./config.json');
 // Create a bot.
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 client.commands = new Collection();
+
+const commands = [];
 
 // Load commands.
 const foldersPath = path.join(__dirname, 'commands');
@@ -26,7 +28,8 @@ for (const folder of fs.readdirSync(foldersPath)){
 		const command = require(filePath);
 
 		if ('data' in command && 'execute' in command){
-			client.commands.set(command.data.name, command);
+			// client.commands.set(command.data.name, command);
+			commands.push(command.data.toJSON());
 			console.log(`${filePath} loaded.`);
 		}
 		else{
@@ -35,8 +38,42 @@ for (const folder of fs.readdirSync(foldersPath)){
 	}
 }
 
+const rest = new REST().setToken(token);
+(async() => {
+	try{
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientID, guildID),
+			{ body: commands },
+		);
+	} catch(error){
+		console.error(error);
+	}
+})();
+
 client.once(Events.ClientReady, readyClient => {
 	console.log('Bot logged!');
+});
+
+client.on(Events.InteractionCreate, async interaction =>{
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command)
+	{
+		console.error(`${interaction.commandName} command doesn't exist!`);
+		return;
+	}
+
+	try{
+		await command.execute(interaction);
+	} catch(error){
+		console.error(error);
+		if (interaction.replied || interaction.deferred){
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
 });
 
 client.login(token);
