6c2ae12f13caa45683349c3b5e65d16a8fb8c6cc
Sebastian Kumór <thekumor@gmail.com>
2025-11-16 11:41:52 +0100

    translatemsg
    + Added translatemsg command.
    * Fixed translate command's description.

--- commands/translatemsg/translatemsg.js
+++ commands/translatemsg/translatemsg.js
@@ -3,8 +3,8 @@ const translate = require('translate-google')
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('translate')
-	.setDescription('Translates text.')
-	.addStringOption((option) => option.setName('text').setDescription('Text to translate').setRequired(true))
+	.setDescription('Translates a message.')
+	.addStringOption((option) => option.setName('message').setDescription('Message (ID) to translate').setRequired(true))
 	.addStringOption((option) => option.setName('from').setDescription('From language').setRequired(true))
 	.addStringOption((option) => option.setName('to').setDescription('To language').setRequired(true)),
 
@@ -13,7 +13,9 @@ module.exports = {
 		const from = interaction.options.getString('from');
 		const to = interaction.options.getString('to');
 
-		translate(text, {from: from, to: to}).then(res => {
+		const message = await interaction.channel.messages.fetch(text);
+
+		translate(message.content, {from: from, to: to}).then(res => {
 			interaction.reply(res);
 		}).catch(err => {
 			console.error(err)
