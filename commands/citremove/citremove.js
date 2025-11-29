44ebfdae9fb2b4cf7eaeb977bbec8ba790024df9
Sebastian Kumór <thekumor@gmail.com>
2025-11-29 15:33:38 +0100

    Update citremove.js

--- commands/citremove/citremove.js
+++ commands/citremove/citremove.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder().setName('citremove')
 	.setDescription('Removes all citations from text (citations are in square brackets [], text is taken from sites like Wikipedia).')
-	.addStringOption((option) => option.setName('message').setDescription('Text to de-citate.').setRequired(true))
+	.addStringOption((option) => option.setName('message').setDescription('Text to de-citate.').setRequired(true)),
 
 	async execute(interaction){
 		var text = interaction.options.getString('message');
