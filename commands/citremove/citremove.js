7a80151ce36bdadfb17056fffbac854f00608c78
Sebastian Kumór <thekumor@gmail.com>
2025-11-29 15:53:49 +0100

    Create citremove.js

---
+++ commands/citremove/citremove.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('citremove')
+	.setDescription('Removes citations from a given text.')
+	.addStringOption((option) => option.setName('text').setDescription('Text to be de-cited.')),
+
+	async execute(interaction){
+		let text = interaction.options.getString('text');
+		let citationPattern = "/\s*\[.*?\]|\s*\(.*?\)|\s*<.*?>/g";
+		text = text.replace(citationPattern, '');
+
+		await interaction.reply(text);
+	}
+};
