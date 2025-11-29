e1ff64911dd5fc16303e8e15c71575fb34c0907c
Sebastian Kumór <thekumor@gmail.com>
2025-11-29 15:55:36 +0100

    Update citremove.js

--- commands/citremove/citremove.js
+++ commands/citremove/citremove.js
@@ -8,7 +8,7 @@ module.exports = {
 
 	async execute(interaction){
 		let text = interaction.options.getString('text');
-		let citationPattern = "/\s*\[.*?\]|\s*\(.*?\)|\s*<.*?>/g";
+		let citationPattern = '/[^a-z0-9-]/g';
 		text = text.replace(citationPattern, '');
 
 		await interaction.reply(text);
