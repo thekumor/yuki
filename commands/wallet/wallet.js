145c618127b02e9832f35387b278f78257bc82d5
Sebastian Kumór <thekumor@gmail.com>
2026-01-02 19:23:20 +0100

    Create wallet.js
    + Added wallet command.

---
+++ commands/wallet/wallet.js
@@ -0,0 +1,28 @@
+// ================================================
+// 
+//	Project: Yuki
+//
+//	File: commands/wallet/wallet.js
+//	Desc: Retrieves user's money from database.
+// 
+//	Modified: 2026/01/02 7:22 PM
+//	Created: 2026/01/02 7:16 PM
+//	Authors: The Kumor
+// 
+// ================================================
+
+const { SlashCommandBuilder } = require('discord.js');
+const { database, guildID } = require('../../config.json');
+const yukidb = require('../../database.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('wallet').setDescription('Retrieves amount of money that you have.'),
+	async execute(interaction){
+		const money = yukidb.Get(interaction.guild.id, interaction.user.id, 'economy', 'money');
+		
+		const serverName = guildID[interaction.guild.id];
+		const currency = database.settings[serverName].currency;
+
+		await interaction.reply('Your money: ' + money + currency);
+	}
+};
