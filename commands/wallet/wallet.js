6160351cfbd83b282d63c56800414881e738070f
Sebastian Kumór <thekumor@gmail.com>
2026-01-02 19:25:43 +0100

    Update wallet.js
    * Made wallet's chat reply wait for database's retrieval of money.
    * Updated wallet command response to contain a space.

--- commands/wallet/wallet.js
+++ commands/wallet/wallet.js
@@ -18,11 +18,11 @@ const yukidb = require('../../database.js');
 module.exports = {
 	data: new SlashCommandBuilder().setName('wallet').setDescription('Retrieves amount of money that you have.'),
 	async execute(interaction){
-		const money = yukidb.Get(interaction.guild.id, interaction.user.id, 'economy', 'money');
+		const money = await yukidb.Get(interaction.guild.id, interaction.user.id, 'economy', 'money');
 		
 		const serverName = guildID[interaction.guild.id];
 		const currency = database.settings[serverName].currency;
 
-		await interaction.reply('Your money: ' + money + currency);
+		await interaction.reply('Your money: ' + money + ' ' + currency);
 	}
 };
