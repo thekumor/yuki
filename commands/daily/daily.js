f1a0afe51bbc1da499016ba5d16169adfb91a9dd
Sebastian Kumór <thekumor@gmail.com>
2026-01-05 00:05:17 +0100

    Update daily.js
    * Fixed daily time checking.

--- commands/daily/daily.js
+++ commands/daily/daily.js
@@ -5,7 +5,7 @@
 //	File: commands/daily/daily.js
 //	Desc: Command for receiving daily money.
 // 
-//	Modified: 2026/01/03 7:13 PM
+//	Modified: 2026/01/05 12:04 AM
 //	Created: 2025/12/26 7:13 PM
 //	Authors: The Kumor
 // 
@@ -29,15 +29,15 @@ module.exports = {
 		);
 		const nextTime = nextMidnight.getTime() / 1000;
 
-		const lastDaily = await yukidb.Get(interaction.guild.id, interaction.user, 'economy', 'lastDaily');
+		const lastNextTime = await yukidb.Get(interaction.guild.id, interaction.user, 'economy', 'lastDaily');
 		const serverName = guildID[interaction.guild.id];
 
 		const currency = database.settings[serverName].currency;
 		const nextTake = nextMidnight.toLocaleString();
 
-		if (nextTime < start) {
+		if (lastNextTime < nextTime) {
 			await yukidb.Add(interaction.guild.id, interaction.user, 'economy', 'money', 500);
-			await yukidb.Set(interaction.guild.id, interaction.user, 'economy', 'lastDaily', start);
+			await yukidb.Set(interaction.guild.id, interaction.user, 'economy', 'lastDaily', nextTime);
 
 			await interaction.reply(`You received 500 ${currency}! Next take: ${nextTake}`);
 		} else {
